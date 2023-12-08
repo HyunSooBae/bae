d685857abac066b91a23a0dc7b635f211b9e1538
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-12-08 18:51:04 +0900

    Feat: firebase 에서 firestore, storage 연결

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 const firebaseConfig = {
   apiKey: "AIzaSyD4O8pe8orEIJkI01ZvNhU67_XGqMIeRY4",
@@ -14,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app)
+
+export const storage = getStorage(app)
+
+export const db = getFirestore(app)
