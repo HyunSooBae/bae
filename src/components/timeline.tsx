33ba1738f20bf59f33891ce575b2d029a101170c
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-12-09 16:29:40 +0900

    Feat: timeline 컴포넌트 생성 및 db데이터 페치해오기 - home 화면에 timeline 컴포넌트 추가

---
+++ src/components/timeline.tsx
@@ -0,0 +1,72 @@
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react'
+import styled from 'styled-components';
+import { db } from '../firebase';
+import Message from './message';
+import { Unsubscribe } from 'firebase/auth';
+
+export interface IMsg {
+  id: string;
+  photo?: string;
+  msg: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+export default function Timeline() {
+  const [msgs, setMsgs] = useState<IMsg[]>([])
+  useEffect(() => {
+    let unsubscribe : Unsubscribe | null = null
+    const fetchMsgs = async () => {
+      const msgsQuery = query(
+        collection(db, "messages"),
+        orderBy("createdAt", "desc"),
+        limit(10)
+      )
+      // const snapshot = await getDocs(msgsQuery)
+      // const msgs = snapshot.docs.map(doc => {
+      //   const { msg, createdAt, userId, username, photo } = doc.data()
+      //   return {
+      //     id: doc.id,
+      //     msg,
+      //     createdAt,
+      //     userId,
+      //     username,
+      //     photo
+      //   }
+      // })
+      unsubscribe = await onSnapshot(msgsQuery, (snapshot) => {
+        const msgs = snapshot.docs.map((doc) => {
+          const { msg, createdAt, userId, username, photo } = doc.data()
+          return {
+            id: doc.id,
+            msg,
+            createdAt,
+            userId,
+            username,
+            photo
+          }
+        })
+        setMsgs(msgs)
+      })
+    }
+    fetchMsgs()
+    return () => { // cleanup function
+      unsubscribe && unsubscribe()
+    }
+  }, [])
+  return (
+    <Wrapper>
+      {msgs.map((msg) => (
+        <Message key={msg.id} {...msg} />
+      ))}
+    </Wrapper>
+  )
+}
