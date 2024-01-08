52d8fafc5b3d580eaf9768f5f94459db7b502efa
HyunSooBae <hyunsoobae.dev@gmail.com>
2024-01-08 18:09:30 +0900

    Feat: 메세지 편집 중 이미지 label id를 고유id로 수정

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -3,7 +3,7 @@ import { IMsg } from './timeline';
 import { auth, db, storage } from '../firebase';
 import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useId, useRef, useState } from 'react';
 
 const Wrapper = styled.div`
   display: grid;
@@ -110,6 +110,7 @@ const AttachFileInput = styled.input`
 `
 
 export default function Message({id, username, photo, msg, userId}: IMsg) {
+  const previewId = useId()
   const [file, setFile] = useState<File | null>(null)
   const [imgUrl, setImgUrl ] = useState(photo)
   // console.log('photo:', photo)
@@ -224,14 +225,14 @@ export default function Message({id, username, photo, msg, userId}: IMsg) {
         <Column>
           <Photo src={imgUrl} /> 
           {editing? (
-            <PhotoUploadButton onClick={onDelClick} htmlFor="editFile">
+            <PhotoUploadButton onClick={onDelClick} htmlFor={`${previewId}-file`}>
               <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clipRule="evenodd" fillRule="evenodd" d="M1 5.25A2.25 2.25 0 0 1 3.25 3h13.5A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17H3.25A2.25 2.25 0 0 1 1 14.75v-9.5Zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-2.69l-2.22-2.219a.75.75 0 0 0-1.06 0l-1.91 1.909.47.47a.75.75 0 1 1-1.06 1.06L6.53 8.091a.75.75 0 0 0-1.06 0l-2.97 2.97ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" />
               </svg>
             </PhotoUploadButton>
           ) : null}
           
-          <AttachFileInput onChange={onFileChange} type="file" id="editFile" accept="image/*" />
+          <AttachFileInput onChange={onFileChange} type="file" id={`${previewId}-file`} accept="image/*" />
         </Column>
       ) : null}
     </Wrapper>
